1e1712bc53bd14e8f81195356fd6f276ac3e8b44
J <jguna0076@launchpadphilly.org>
2025-05-16 10:59:03 -0400

    vercel oauth fix

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -1,19 +1,12 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
-const options = {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-};
 
-let client;
-let clientPromise;
-
-if (!process.env.MONGODB_URI) {
-    throw new Error('Please add your MONGODB_URI environment variable');
+if (!uri) {
+  throw new Error("Please add your MONGODB_URI environment variable");
 }
 
-client = new MongoClient(uri, options);
-clientPromise = client.connect();
+const client = new MongoClient(uri);
+const clientPromise = client.connect();
 
 export default clientPromise;
